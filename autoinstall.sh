8bad54d0240db7e7625b4745b6e1e16eb803945d
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-02-05 13:05:15 -0300

    Criado o autoinstall.sh

---
+++ autoinstall.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Auto Instalador do agreSSive Framework
+# Um sistema completo para criação e administração de rádios usando o ShoutCast e sc-trans!
+# Por: Lucas Saliés Brum, a.k.a. sistematico <lucas AT archlinux DOT com DOT br>
+# Criado em 05/02/2017
+# Alterado em 05/02/2017
+
+# Em breve...
