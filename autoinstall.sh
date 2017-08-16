6729d369a61ef117168eb4ec2358e1e1622883f3
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-08-16 16:17:46 -0400

    modified:   autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -482,18 +482,18 @@ fi
 cat <<EOF > ${webpath}/conf/config.php
 <?php
 
-$amin = 3;
-$tmin = 5;
+\$amin = 3;
+\$tmin = 5;
 
-$musicaspath = '${TRANS_PATH}';
-$dbpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db';
-$capa_padrao 		= '/img/logotipo.svg';
-$lastfm_api 		= 'CRIE_SUA_API';
-$shoutcast_url	= 'http://${ip}';
-$shoutcast_port	= '${porta}';
-$extensoes = array('mp3');
+\$musicaspath = '${TRANS_PATH}';
+\$dbpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db';
+\$capa_padrao 		= '/img/logotipo.svg';
+\$lastfm_api 		= 'CRIE_SUA_API';
+\$shoutcast_url	= 'http://${ip}';
+\$shoutcast_port	= '${porta}';
+\$extensoes = array('mp3');
 
-$acentos = array(
+\$acentos = array(
     'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
     'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
     'Ï'=>'I', 'Ñ'=>'N', 'Ń'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
@@ -504,24 +504,24 @@ $acentos = array(
     'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S',
 );
 
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
-    $ip = $_SERVER['REMOTE_ADDR'];
+if (!empty(\$_SERVER['HTTP_CLIENT_IP'])) {
+    \$ip = \$_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty(\$_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    \$ip = \$_SERVER['HTTP_X_FORWARDED_FOR'];
+} elseif (!empty(\$_SERVER['REMOTE_ADDR'])) {
+    \$ip = \$_SERVER['REMOTE_ADDR'];
 } else {
-  $ip = "0.0.0.0";
+  \$ip = "0.0.0.0";
 }
 
 function comparaTempo($tempo) {
-  $agora = time();
-  $mins = ($agora-$tempo) / 60;
-  return floor(abs($mins));
+  \$agora = time();
+  \$mins = (\$agora-\$tempo) / 60;
+  return floor(abs(\$mins));
 }
 
-$db = new PDO('sqlite:' . dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db/agressive.sqlite') or die("Impossivel criar BD");
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+\$db = new PDO('sqlite:' . dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db/agressive.sqlite') or die("Impossivel criar BD");
+\$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
 EOF
