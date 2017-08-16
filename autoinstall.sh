c53361f6059be48eb1b12b07d3efb076de25e3b6
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-08-16 15:03:53 -0400

    modified:   autoinstall.sh 	new file:   web/conf/config.bkp.php

--- autoinstall.sh
+++ autoinstall.sh
@@ -216,6 +216,7 @@ echo "Senha: $senha"
 echo "Sistema: ${caminho}"
 echo "Config: /etc/agressive/config"
 echo "Web: ${webpath}"
+echo "Caminho das músicas: ${TRANS_PATH}"
 echo "---------------------------------------------"
 
 while true; do
@@ -228,9 +229,7 @@ while true; do
   esac
 done
 
-id "${usuario}" 1> /dev/null 2> /dev/null
-
-if [ $? ]; then
+if [[ ! $(id ${usuario} 1> /dev/null 2> /dev/null) ]]; then
   echo
   echo "Criando o usuário: ${usuario}..."
   useradd -r -m -G "${grupo_web}" -c "agreSSive User" -s /bin/bash -p "${senha}" "${usuario}"
@@ -451,7 +450,7 @@ read -p "Site da Rádio [Padrão: https://sistematico.github.io/agressive] " sit
 read -p "Senha da fonte [Padrão: sourcepasswd] " sourcepasswd
 read -p "Senha do admin [Padrão: adminpasswd] " adminpasswd
 read -p "Gênero [Padrão: Misc] " genero
-read -p "IP [Padrão: localhost] " ip
+read -p "IP/Domínio [Padrão: localhost] " ip
 read -p "Porta [Padrão: 8000] " porta
 read -p "Bitrate [Padrão: 128000] " bitrate
 read -p "mp3 ou aac? [Padrão: aac] " tipo
@@ -479,6 +478,53 @@ unlockkeyname=${lnome}
 unlockkeycode=${lkey}
 EOM
 fi
+
+cat <<EOF > ${webpath}/conf/config.php
+<?php
+
+$amin = 3;
+$tmin = 5;
+
+$musicaspath = '${TRANS_PATH}';
+$dbpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db';
+$capa_padrao 		= '/img/logotipo.svg';
+$lastfm_api 		= 'CRIE_SUA_API';
+$shoutcast_url	= 'http://${ip}';
+$shoutcast_port	= '${porta}';
+$extensoes = array('mp3');
+
+$acentos = array(
+    'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+    'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+    'Ï'=>'I', 'Ñ'=>'N', 'Ń'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+    'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+    'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+    'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ń'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+    'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f', '&'=>'e',
+    'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S',
+);
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+} else {
+  $ip = "0.0.0.0";
+}
+
+function comparaTempo($tempo) {
+  $agora = time();
+  $mins = ($agora-$tempo) / 60;
+  return floor(abs($mins));
+}
+
+$db = new PDO('sqlite:' . dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db/agressive.sqlite') or die("Impossivel criar BD");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
+EOF
 
 
 cat <<EOF > ${SHOUT_HOME}/sc_serv_agressive.conf
---
+++ web/conf/config.bkp.php
@@ -0,0 +1,44 @@
+<?php
+
+$amin = 3;
+$tmin = 5;
+
+$musicaspath = '/usr/local/musicas';
+$dbpath = dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db';
+$capa_padrao 		= '/img/logotipo.svg';
+$lastfm_api 		= 'CRIE_SUA_API';
+$shoutcast_url	= 'http://site.com';
+$shoutcast_port	= '8000';
+$extensoes = array('mp3');
+
+$acentos = array(
+    'Š'=>'S', 'š'=>'s', 'Ð'=>'Dj','Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A',
+    'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E', 'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I',
+    'Ï'=>'I', 'Ñ'=>'N', 'Ń'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U', 'Ú'=>'U',
+    'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss','à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a',
+    'å'=>'a', 'æ'=>'a', 'ç'=>'c', 'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i',
+    'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ń'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o', 'ö'=>'o', 'ø'=>'o', 'ù'=>'u',
+    'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y', 'ƒ'=>'f', '&'=>'e',
+    'ă'=>'a', 'î'=>'i', 'â'=>'a', 'ș'=>'s', 'ț'=>'t', 'Ă'=>'A', 'Î'=>'I', 'Â'=>'A', 'Ș'=>'S',
+);
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} elseif (!empty($_SERVER['REMOTE_ADDR'])) {
+    $ip = $_SERVER['REMOTE_ADDR'];
+} else {
+  $ip = "0.0.0.0";
+}
+
+function comparaTempo($tempo) {
+  $agora = time();
+  $mins = ($agora-$tempo) / 60;
+  return floor(abs($mins));
+}
+
+$db = new PDO('sqlite:' . dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'db/agressive.sqlite') or die("Impossivel criar BD");
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
