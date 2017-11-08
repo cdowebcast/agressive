a43e2e6aa7af7aae0bae7cdf63de5945a9053bb6
Lucas Saliés Brum <lucas@archlinux.com.br>
2017-11-08 00:39:13 -0300

    modified:   .travis.yml 	modified:   web/php/engine.php

--- web/php/engine.php
+++ web/php/engine.php
@@ -1,11 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-//ini_set('display_errors', 'On');
-//error_reporting(E_ALL);
-
->>>>>>> 6ec914b47969855cc107c9d674d640cb7b3579af
 include(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'conf/config.php');
 
 $resultado=$db->prepare("SELECT COUNT(*) FROM pedidos");
@@ -14,7 +8,6 @@ $registros = $resultado->fetchColumn();
 
 if ($registros > 0) {
 	try {
-<<<<<<< HEAD
 		$result = $db->prepare('SELECT * FROM pedidos ORDER BY hora LIMIT 1');
 		$result->execute();
 		$row = $result->fetch();
@@ -25,23 +18,20 @@ if ($registros > 0) {
 		$db = NULL;
 	 } catch(PDOException $e) {
 		print 'Exception : '.$e->getMessage();
-=======
-	   $result = $db->prepare('SELECT * FROM pedidos ORDER BY hora LIMIT 1');
-	   $result->execute();
-		 $row = $result->fetch();
-		 print($row['caminho']);
-		 $result = $db->prepare('DELETE FROM pedidos WHERE id=:id LIMIT 1');
-		 $result->bindValue(':id', $row['id']);
-		 $result->execute();
-		 $db = NULL;
+		$result = $db->prepare('SELECT * FROM pedidos ORDER BY hora LIMIT 1');
+		$result->execute();
+		$row = $result->fetch();
+		print($row['caminho']);
+		$result = $db->prepare('DELETE FROM pedidos WHERE id=:id LIMIT 1');
+		$result->bindValue(':id', $row['id']);
+		$result->execute();
+		$db = NULL;
 	 } catch(PDOException $e) {
-	   print 'Exception : '.$e->getMessage();
->>>>>>> 6ec914b47969855cc107c9d674d640cb7b3579af
+		print 'Exception : '.$e->getMessage();
 	 }
 } else {
 	// AutoDJ
 	try {
-<<<<<<< HEAD
 		$result = $db->prepare('SELECT caminho FROM musicas WHERE id >= (abs(random()) % (SELECT max(id) FROM musicas)) LIMIT 1');
 		$result->execute();
 		$row = $result->fetch( PDO::FETCH_ASSOC );
@@ -50,15 +40,13 @@ if ($registros > 0) {
 	} catch(PDOException $e) {
 		print 'Exception : '.$e->getMessage();
 	}
-=======
-		 $result = $db->prepare('SELECT caminho FROM musicas WHERE id >= (abs(random()) % (SELECT max(id) FROM musicas)) LIMIT 1');
-	   $result->execute();
-	   $row = $result->fetch( PDO::FETCH_ASSOC );
-	   print($row['caminho']);
-	   $db = NULL;
-	 } catch(PDOException $e) {
-	   print 'Exception : '.$e->getMessage();
-	 }
->>>>>>> 6ec914b47969855cc107c9d674d640cb7b3579af
+		$result = $db->prepare('SELECT caminho FROM musicas WHERE id >= (abs(random()) % (SELECT max(id) FROM musicas)) LIMIT 1');
+		$result->execute();
+		$row = $result->fetch( PDO::FETCH_ASSOC );
+		print($row['caminho']);
+		$db = NULL;
+	} catch(PDOException $e) {
+		print 'Exception : '.$e->getMessage();
+	}
 }
 ?>
